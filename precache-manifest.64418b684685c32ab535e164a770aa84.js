a69559249e7775c39c1d7bf6603cedfc3c5beff1
zhangjun <jun.zhang@convertlab.com>
2020-05-25 17:25:20 +0800

    Updates

---
+++ precache-manifest.64418b684685c32ab535e164a770aa84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b34c0109cf4528ebbf01ee682afc8b65",
+    "url": "/webpacktest/index.html"
+  },
+  {
+    "revision": "f7f2c61833e52535f03c",
+    "url": "/webpacktest/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8b5080807878e6f69f7a",
+    "url": "/webpacktest/static/js/2.59c685af.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/webpacktest/static/js/2.59c685af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7f2c61833e52535f03c",
+    "url": "/webpacktest/static/js/main.37829982.chunk.js"
+  },
+  {
+    "revision": "67329d8a6eb8a8bc26ae",
+    "url": "/webpacktest/static/js/runtime-main.181c02fa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/webpacktest/static/media/logo.5d5d9eef.svg"
+  }
+]);
